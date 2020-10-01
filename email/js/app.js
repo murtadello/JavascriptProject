e94e301895331851e150cfabdda7cd1d06c8df5e
Murtaza <murtaza.mohammadi@powercoders.org>
2020-10-01 11:11:56 +0200

    This is the first

--- email/js/app.js
+++ email/js/app.js
@@ -1,108 +1,112 @@
 //variables
 
-const sendBtn = document.getElementById('sendBtn'),
-      email = document.getElementById('email'),
-      subject = document.getElementById('subject'),
-      message = document.getElementById('message'),
-      resetBtn = document.getElementById('resetBtn'),
-      sendEmailForm = document.getElementById('email-form');
+const sendBtn = document.getElementById("sendBtn"),
+  email = document.getElementById("email"),
+  subject = document.getElementById("subject"),
+  message = document.getElementById("message"),
+  resetBtn = document.getElementById("resetBtn"),
+  sendEmailForm = document.getElementById("email-form");
 //Event Listeners
 eventListeners();
- 
-function eventListeners(){
-    //App init
-    document.addEventListener('DOMContentLoaded', appInit);
-    email.addEventListener('blur', validateField);
-    subject.addEventListener('blur', validateField);
-    message.addEventListener('blur', validateField);
-    //send email and  reset button
-    sendEmailForm.addEventListener('submit', sendEmail);
-    resetBtn.addEventListener('click', resetForm);  
+
+function eventListeners() {
+  //App init
+  document.addEventListener("DOMContentLoaded", appInit);
+  email.addEventListener("blur", validateField);
+  subject.addEventListener("blur", validateField);
+  message.addEventListener("blur", validateField);
+  //send email and  reset button
+  sendEmailForm.addEventListener("submit", sendEmail);
+  resetBtn.addEventListener("click", resetForm);
 }
+
 //Functions
 //App intialization
-function appInit(){
-    //disable the send button on page loaded
-    sendBtn.disabled = true;
+function appInit() {
+  //disable the send button on page loaded
+  sendBtn.disabled = true;
 }
 
-function sendEmail(e){
-    e.preventDefault();
+function sendEmail(e) {
+  e.preventDefault();
 
-    //show spinner
-    const spinner = document.querySelector('#spinner');
-    spinner.style.display ='block';
+  //show spinner
+  const spinner = document.querySelector("#spinner");
+  spinner.style.display = "block";
 
+  //show the image
+  const sendEmailImg = document.createElement("img");
+  sendEmailImg.src = "img/mail.gif";
+  sendEmailImg.style.display = "block";
+
+  //Hide spinner and show the send Email image
+  setTimeout(() => {
+    //Hide the spinner
+    spinner.style.display = "none";
     //show the image
-    const sendEmailImg = document.createElement('img');
-    sendEmailImg.src ='img/mail.gif';
-    sendEmailImg.style.display = 'block';
+    document.querySelector("#loaders").appendChild(sendEmailImg);
 
-    //Hide spinner and show the send Email image
+    //After 5 seconds , hide the image and reset the from
     setTimeout(() => {
-        //Hide the spinner
-        spinner.style.display = 'none';
-        //show the image
-        document.querySelector('#loaders').appendChild(sendEmailImg);
-        
-        //After 5 seconds , hide the image and reset the from
-        setTimeout(() => {
-            sendEmailForm.reset();
-            sendEmailImg.remove();
-        }, 5000);
-    
-    }, 3000);
-
-
+      sendEmailForm.reset();
+      sendEmailImg.remove();
+    }, 5000);
+  }, 3000);
 }
 //Validate the fields
-function validateField(){
-    let errors;
-    //validate the length of the field;
-    validateLength(this);
-    //validate the email
-    if(this.type === 'email'){
-        validateEmail(this);
-    }
+function validateField() {
+  let errors;
+  //validate the length of the field;
+  validateLength(this);
+  //validate the email
+  if (this.type === "email") {
+    validateEmail(this);
+  }
 
-    //Both will return erros, then check if there is any erros
-    errors = document.querySelectorAll('.error');
-    // check that the inputs are not empty
-    if(email.value !== ''  && subject.value !== ''&& message !== ''){
-        if(errors.length === 0){
-            sendBtn.disabled = false;
-        }
+  //Both will return erros, then check if there is any erros
+  errors = document.querySelectorAll(".error");
+  // check that the inputs are not empty
+  if (email.value !== "" && subject.value !== "" && message !== "") {
+    if (errors.length === 0) {
+      sendBtn.disabled = false;
     }
+  }
 }
 //validate the lenght of the fields
 
-function validateLength(field){
-    if(field.value.length > 0){
-        field.style.borderBottomColor = 'green';
-        field.classList.remove('error');
-    }else {
-        field.style.borderBottomColor = 'red';
-        field.classList.add('error');
-    }
+function validateLength(field) {
+  if (field.value.length > 0) {
+    field.style.borderBottomColor = "green";
+    field.classList.remove("error");
+  } else {
+    field.style.borderBottomColor = "red";
+    field.classList.add("error");
+  }
 }
 
 //validate email (check for @ inn the value)
 
-function validateEmail(field){
-    let emailText = field.value;
-    // check if the emailText contains @
-    if(emailText.indexOf('@') !== -1){
-        field.style.borderBottomColor = 'green';
-        field.classList.remove('error');
-    }else {
-        field.style.borderBottomColor = 'red';
-        field.classList.add('error');
-    }
+function validateEmail(field) {
+  let emailText = field.value;
+  // check if the emailText contains @
+  if (emailText.indexOf("@") !== -1) {
+    field.style.borderBottomColor = "green";
+    field.classList.remove("error");
+  } else {
+    field.style.borderBottomColor = "red";
+    field.classList.add("error");
+  }
 }
-
 //Reset the Form
-
-function resetForm(){
-    sendEmailForm.reset();
+function resetForm() {
+  sendEmailForm.reset();
 }
 
+function sendrequest() {
+  const xml = document.getElementById("industy").value;
+  xml.addEventListener(function (e) {
+    console.log(e.classList);
+    console.log(e.target.classList);
+    console.log(e.createElement("p"));
+  });
+}
--- shoppingcart/js/app.js
+++ shoppingcart/js/app.js
@@ -1,53 +1,49 @@
 //Variabels
-const courses = document.querySelector('#courses-list'),
-      shoppingCartContent = document.querySelector('#cart-content tbody'),
-      clearCartBtn = document.querySelector('#clear-cart');  
+const courses = document.querySelector("#courses-list"),
+  shoppingCartContent = document.querySelector("#cart-content tbody"),
+  clearCartBtn = document.querySelector("#clear-cart");
 
 //Listeners
 loadEventListeners();
-function loadEventListeners(){
-    //when a new course is added
-    courses.addEventListener('click', buyCourse);
-    //when a the remove button is clicked
-    shoppingCartContent.addEventListener('click',removeCourse);
-    //clear Cart Btn
-    clearCartBtn.addEventListener('click', clearCart);
-    //Document Ready
-    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
+function loadEventListeners() {
+  //when a new course is added
+  courses.addEventListener("click", buyCourse);
+  //when a the remove button is clicked
+  shoppingCartContent.addEventListener("click", removeCourse);
+  //clear Cart Btn
+  clearCartBtn.addEventListener("click", clearCart);
+  //Document Ready
+  document.addEventListener("DOMContentLoaded", getFromLocalStorage);
 }
 //Functions
 
-function buyCourse(e){
-    e.preventDefault();
-    //use delegationn to find the course that was added
-    if(e.target.classList.contains('add-to-cart')){
-        
-        const course = e.target.parentElement.parentElement;
-        //read the value
-        getCourseInfo(course);
-        
-    }
-    
+function buyCourse(e) {
+  e.preventDefault();
+  //use delegationn to find the course that was added
+  if (e.target.classList.contains("add-to-cart")) {
+    const course = e.target.parentElement.parentElement;
+    //read the value
+    getCourseInfo(course);
+  }
 }
 
 //Read the HTML information of the selected course
 
-function getCourseInfo(course){
-    const courseInfo = {
-        image:course.querySelector('img').src,
-        title:course.querySelector('h4'),
-        price:course.querySelector('.price span').textContent,
-        id: course.querySelector('a').getAttribute('data-id')
-
-    }
-    addIntoCart(courseInfo);
+function getCourseInfo(course) {
+  const courseInfo = {
+    image: course.querySelector("img").src,
+    title: course.querySelector("h4"),
+    price: course.querySelector(".price span").textContent,
+    id: course.querySelector("a").getAttribute("data-id"),
+  };
+  addIntoCart(courseInfo);
 }
 
-function addIntoCart(course){
-    //Create a <tr>
-    const row = document.createElement('tr');
+function addIntoCart(course) {
+  //Create a <tr>
+  const row = document.createElement("tr");
 
-    row.innerHTML = `
+  row.innerHTML = `
     <tr>
         <td>
             <img src="${course.image}" width = 100>
@@ -62,98 +58,89 @@ function addIntoCart(course){
     </tr>
     
     `;
-    //Add into the shopping cart
-    shoppingCartContent.appendChild(row);
-    //Add course into Storage
+  //Add into the shopping cart
+  shoppingCartContent.appendChild(row);
+  //Add course into Storage
 
-    saveIntoStorage(course);
+  saveIntoStorage(course);
 }
 //Add the course into the local storage
 
-function saveIntoStorage(course){
-    let courses = getCourseFromStorage();
-    //Add the course into the array
-    courses.push(course);
-    //since storage only saves string we need to convert JSON into string
-    localStorage.setItem('courses', JSON.stringify(courses));
-
+function saveIntoStorage(course) {
+  let courses = getCourseFromStorage();
+  //Add the course into the array
+  courses.push(course);
+  //since storage only saves string we need to convert JSON into string
+  localStorage.setItem("courses", JSON.stringify(courses));
 }
 //Get the content from the storage
 
-function getCourseFromStorage(){
-    let courses;
+function getCourseFromStorage() {
+  let courses;
 
-    //if something exist on storage then we get the value, otherwise we create an empty arrar
-    if(localStorage.getItem('courses') === null){
-        courses = [];
-    }else{
-        courses = JSON.parse(localStorage.getItem('courses'));
-    }
-    return courses;
-
+  //if something exist on storage then we get the value, otherwise we create an empty arrar
+  if (localStorage.getItem("courses") === null) {
+    courses = [];
+  } else {
+    courses = JSON.parse(localStorage.getItem("courses"));
+  }
+  return courses;
 }
-
 
 //Remove course from the dom
 
-function removeCourse(e){
-    let course, courseId;
-    //remove from the dom
-    if(e.target.classList.contains('remove')) {
-        e.target.parentElement.parentElement.remove();
-        course = e.target.parentElement.parentElement;
-        courseId = course.querySelector('a').getAttribute('data-id');
-    }
-    console.log(courseId);
-    //remove from the loacal storage
-    removeCourseLocalStorage(courseId);
-
+function removeCourse(e) {
+  let course, courseId;
+  //remove from the dom
+  if (e.target.classList.contains("remove")) {
+    e.target.parentElement.parentElement.remove();
+    course = e.target.parentElement.parentElement;
+    courseId = course.querySelector("a").getAttribute("data-id");
+  }
+  console.log(courseId);
+  //remove from the loacal storage
+  removeCourseLocalStorage(courseId);
 }
 //remove form local storage
-function removeCourseLocalStorage(id){
-    //get the local storage data
-    let coursesLS = getCourseFromStorage();
-    //loop through the array and find the index to remov
-    coursesLS.forEach(function(courseLS, index){
-        if(courseLS.id === id){
-            coursesLS.splice(index, 1);
-        }
-    });
-    //Add the rest of the arary
-    localStorage.setItem('courses', JSON.stringify(coursesLS))
-    
-
+function removeCourseLocalStorage(id) {
+  //get the local storage data
+  let coursesLS = getCourseFromStorage();
+  //loop through the array and find the index to remov
+  coursesLS.forEach(function (courseLS, index) {
+    if (courseLS.id === id) {
+      coursesLS.splice(index, 1);
+    }
+  });
+  //Add the rest of the arary
+  localStorage.setItem("courses", JSON.stringify(coursesLS));
 }
 
 //clear the shopping cart
 
-
-function clearCart(e){
-    while(shoppingCartContent.firstChild){
-        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
-    }
-    //clear from loaca storage
-    clearLocalStorage();
-
+function clearCart(e) {
+  while (shoppingCartContent.firstChild) {
+    shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+  }
+  //clear from loaca storage
+  clearLocalStorage();
 }
 
 // clear the whole local storage
 
-function clearLocalStorage(){
-    localStorage.clear();
+function clearLocalStorage() {
+  localStorage.clear();
 }
-
 
 //Load when document is ready and print the course into shopping cart
 
-function getFromLocalStorage(){
-    let coursesLS = getCourseFromStorage();
+function getFromLocalStorage() {
+  let coursesLS = getCourseFromStorage();
 
-    coursesLS.forEach(function (course){
-        const row = document.createElement('tr');
+  coursesLS.forEach(function (course) {
+    const row = document.createElement("tr");
 
-        //print the content
-        row.innerHTML = `
+    //print the content
+    row.innerHTML = `
         <tr>
             <td>
                 <img src="${course.image}" width = 100>
@@ -168,6 +155,6 @@ function getFromLocalStorage(){
         </tr>
     
         `;
-        shoppingCartContent.appendChild(row);
-    });
+    shoppingCartContent.appendChild(row);
+  });
 }
